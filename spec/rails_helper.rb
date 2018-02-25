0c69692aa31fc2ceedd047222ec46547c9c2f0bd
ryosuke1356 <soccer1356abc@gmail.com>
2018-02-25 16:11:30 +0900

    Fix pointing outs of rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,8 @@ require 'rspec/rails'
 require 'factory_girl_rails'
 
 # Dir[Rails.root.join('spec/factories/*.rb')].each {|f| require f }
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
