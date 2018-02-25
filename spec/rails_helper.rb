504c746d2dc59cbb950d53dd4ae8801b2c483d35
ryosuke1356 <soccer1356abc@gmail.com>
2018-02-25 15:49:03 +0900

    Rename user_factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'factory_girl_rails'
 
-Dir[Rails.root.join('spec/factories/*.rb')].each {|f| require f }
+# Dir[Rails.root.join('spec/factories/*.rb')].each {|f| require f }
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
