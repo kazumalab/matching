1f8b6bdb16e28ecc2e8fafda9802cc68bb3b3f72
ryosuke1356 <soccer1356abc@gmail.com>
2018-03-04 22:30:04 +0900

    Fix colum name of user_profile

--- db/migrate/20180228143806_create_user_profiles.rb
+++ db/migrate/20180228143806_create_user_profiles.rb
@@ -14,8 +14,8 @@ class CreateUserProfiles < ActiveRecord::Migration[5.1]
       t.integer :smoking,                limit: 1, default: 0, null: false, unsigned: true # タバコ 0:未選択
       t.integer :marriage_history,       limit: 1, default: 0, null: false, unsigned: true # 婚歴 0:未選択
       t.integer :marriage_intention,     limit: 1, default: 0, null: false, unsigned: true # 結婚に対する意思 0:未選択
-      t.integer :date_cost_at_first,     limit: 1, default: 0, null: false, unsigned: true # 初デートの費用 0:未選択
-      t.integer :date_cost_after_second, limit: 1, default: 0, null: false, unsigned: true # 2回目以降のデートの費用 0:未選択
+      t.integer :first_date_cost,        limit: 1, default: 0, null: false, unsigned: true # 初デートの費用 0:未選択
+      t.integer :after_first_date_cost,  limit: 1, default: 0, null: false, unsigned: true # 2回目以降のデートの費用 0:未選択
 
       t.timestamps
     end
