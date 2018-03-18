fa9c30132e6c7182845ed3f50a4fd0f9a5e44137
ryosuke1356 <soccer1356abc@gmail.com>
2018-03-18 17:47:31 +0900

    Refactoring test of sessions_controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  let(:user) { build(:user) }
+  let(:user) { create(:user) }
   let(:facebook) { "facebook" }
+
   before do
     sign_in_as(facebook, user)
   end
@@ -16,7 +17,9 @@ RSpec.describe SessionsController, type: :controller do
   describe "#destroy" do
     before { get :destroy, params: { session: { user_id: user.id } } }
 
-    it { expect(response).to have_http_status 302 }
-    it { expect(response).to redirect_to root_path }
+    it :aggregate_failures do
+      expect(response).to have_http_status 302
+      expect(response).to redirect_to root_path
+    end
   end
 end
